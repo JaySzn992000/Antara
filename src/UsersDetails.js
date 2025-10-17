ce1ecb47eb550d6905c85f040fb8a7966dfd9e86
JaySzn992000 <121809806+JaySzn992000@users.noreply.github.com>
2025-10-17 23:55:00 +0530

    Update UsersDetails.js

--- src/UsersDetails.js
+++ src/UsersDetails.js
@@ -14,7 +14,7 @@ useEffect(() => {
 const DbFetch = async () => {
 try {
 
-const response = await fetch("https://lavonne-0729.onrender.com/usersDetails");
+const response = await fetch("https://antara-gug4.onrender.com/usersDetails");
 
 const data = await response.json();
 
