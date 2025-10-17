6b9264aaaceac70a4225ea9f922bf0cc86e9dcf7
JaySzn992000 <121809806+JaySzn992000@users.noreply.github.com>
2025-10-17 23:50:28 +0530

    Update AdminForget.js

--- src/AdminForget.js
+++ src/AdminForget.js
@@ -45,7 +45,7 @@ return;
 }
 
 try {
-const response = await fetch("https://lavonne-0729.onrender.com/resetAdminPassword", {
+const response = await fetch("https://antara-gug4.onrender.com/resetAdminPassword", {
 method: "POST",
 headers: {
 "Content-Type": "application/json",
