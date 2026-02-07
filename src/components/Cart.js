5604991760d4577750b09c3f1e164bd314ab02d0
JaySzn992000 <121809806+JaySzn992000@users.noreply.github.com>
2026-02-07 22:05:09 +0530

    Update Cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -78,7 +78,7 @@ quantities,
 
 const getProductImagePath = (productId) => {
 const product = productData.find((p) => p.id === productId);
-return product ? `https://antara-gug4.onrender.com${product.file_path}` : "";
+return product ? product.file_path : "";
 };
 
 
